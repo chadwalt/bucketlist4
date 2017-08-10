e9c30f3a1edf3543360995de9a6f54abd1dae6d8
chadwalt <chadwalt@outlook.com>
2017-08-10 07:32:07 +0300

    Restructuring the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,30 +5,29 @@ import React, { Component } from 'react';
 
 
 class Login extends Component{
-  render(
+  render(){
     return(
-      <button>Bucket List App</button>
-      <div className="form">
-      <form action="/login"method="POST"                                                                                                                                                                     git >
-        <div className="forceColor"></div>
-        <div className="topbar">
-          <div className="spanColor"></div>
-          <input type="text" className="input" name="username" id="username" placeholder="Username" />
-        </div>
-        <div className="topbar">
-          <div className="spanColor"></div>
-          <input type="password" className="input" id="password" placeholder="Password" name="password"/>
-        </div>
+      <div>
+        <button>Bucket List App</button>
+        <div className="form">
+          <form action="login"method="POST">
+            <div className="forceColor"></div>
+            <div className="topbar">
+              <div className="spanColor"></div>
+              <input type="text" className="input" name="username" id="username" placeholder="Username" />
+            </div>
 
-        <button className="submit" name="submit" type="submit" id="submit">Login</button>
+            <div className="topbar">
+             <div className="spanColor"></div>
+             <input type="password" className="input" id="password" placeholder="Password" name="password"/>
+           </div>
+            <button className="submit" name="submit" type="submit" id="submit">Login</button>
+            <button className="submit" id="signupAccount">Create Account</button>
         </form>
-        <br>
-        <button className="submit" id="signupAccount">Create Account</button>
-
+       </div>
       </div>
-    </form>
     );
-  );
+  };
 }
 
 export default Login;
