9408c449f2c049ba20c9d5ea713d098e2329833d
chadwalt <chadwalt@outlook.com>
2017-08-21 18:14:46 +0300

    Added routes and removed some bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login'
+import Signup from './components/Signup'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <Switch>
+      <Route exact path='/' component={Login}/>
+      <Route path='/signup' component={Signup}/>
+    </Switch>
   </BrowserRouter>), document.getElementById('root'));
 registerServiceWorker();
