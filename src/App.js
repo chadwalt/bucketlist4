d30d6ccdd2b911232da91f795112df9427cf1812
chadwalt <chadwalt@outlook.com>
2017-08-21 18:12:57 +0300

    Chaning the route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
+import {Router, Route} from 'react-router'
 
 class App extends Component {
   render() {
     return (
-      <Login/>
+      //<Login/>
+      <Router>
+        <Route path='/' component={Login}/>
+      </Router>
     );
   }
 }
