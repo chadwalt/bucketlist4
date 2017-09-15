d3f434a8b6871fe2a0fb0df01fa2b11221dfb7b6
chadwalt <chadwalt04@gmail.com>
2017-09-15 12:22:18 +0300

    Use eslint best practises

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './components/Login'
-import Signup from './components/Signup'
-import registerServiceWorker from './registerServiceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render((
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={Login}/>
-      <Route path='/signup' component={Signup}/>
-    </Switch>
-  </BrowserRouter>), document.getElementById('root'));
-registerServiceWorker();
+	<BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={Login}/>
+			<Route path="/signup" component={Signup}/>
+		</Switch>
+	</BrowserRouter>), document.getElementById("root"))
+registerServiceWorker()
