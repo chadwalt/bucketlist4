089caf75245860730f896e203b1c69c5253c8e6b
chadwalt <chadwalt04@gmail.com>
2017-09-19 14:33:52 +0300

    Remove Buckets route

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Login from "./components/Login"
-import Signup from "./components/Signup"
-import Bucketlist from "./components/Bucketlist"
+import Login from "./containers/Login"
+import Signup from "./containers/Signup"
 import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render((
 	<BrowserRouter>
 		<Switch>
 			<Route exact path="/" component={Login}/>
-			<Route path="/signup" component={Signup}/>
-			<Route path="/dashboard" component={Bucketlist}/>
+			<Route path="/signup" component={Signup}/>			
 		</Switch>
 	</BrowserRouter>), document.getElementById("root"))
 registerServiceWorker()
