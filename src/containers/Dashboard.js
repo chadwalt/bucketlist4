94c9615de5312fdcc6b78abb5cd2f684a711e905
chadwalt <chadwalt04@gmail.com>
2017-09-20 11:29:44 +0300

    Add route and render for logout

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -4,20 +4,32 @@
 
 import React, { Component } from "react"
 import Buckets from "../components/buckets"
+import { Redirect } from "react-router"
 
 class Dashboard extends Component {
 	constructor(props) {
 		super(props)
 
 		this.state = { // Set the current state of this prop.
-			bucket: ""
+			bucket: "",
+			redirect: ""
 		}
 	}
 
 	render() {
-		return (
-			<Buckets/>
-		)
+		const { redirect } = this.state
+
+		if (redirect === "logout") {
+			// Delete the token in the session Storage.
+			sessionStorage.removeItem("token")
+			return (<Redirect to="/" />) // Redirect to login.
+		} else if (redirect === "" || sessionStorage.token) {
+			return (
+				<Buckets />
+			)
+		}
+
+
 	}
 }
 
