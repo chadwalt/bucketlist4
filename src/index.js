ee12063cb5ca255cd3642d2f7fba1da53ca55b40
chadwalt <chadwalt04@gmail.com>
2017-09-20 11:07:31 +0300

    Add Dashboard route

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import "./index.css"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Login from "./containers/Login"
 import Signup from "./containers/Signup"
+import Dashboard from "./containers/Dashboard"
 import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render((
 	<BrowserRouter>
 		<Switch>
 			<Route exact path="/" component={Login}/>
-			<Route path="/signup" component={Signup}/>			
+			<Route path="/signup" component={Signup}/>		
+			<Route path="/dashboard" component={Dashboard}/>			
 		</Switch>
 	</BrowserRouter>), document.getElementById("root"))
 registerServiceWorker()
