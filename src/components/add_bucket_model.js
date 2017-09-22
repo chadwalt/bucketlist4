d8a7f38e0e6306b5a3e9706f724bf12323a3e812
chadwalt <chadwalt04@gmail.com>
2017-09-22 10:24:28 +0300

    Add alert and confirm models for buckets

--- src/components/add_bucket_model.js
+++ src/components/add_bucket_model.js
@@ -23,5 +23,48 @@ const AddBucketModal = (props) => (
 
 )
 
-export default AddBucketModal
+const AlertModel = (props) => (
+	<div className="modal fade" id="alertModal" tabIndex="-1" role="dialog" aria-labelledby="myAlertLabel" aria-hidden="false">
+		<div className="modal-dialog">
+			<div className="modal-content">
+				<div className="modal-header">
+					<button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 className="modal-title" id="myModalLabel">{props.title}</h4>
+				</div>
+				<div className="modal-body">
+					<p>
+						{props.alert_message}
+					</p>
+				</div>
+				<div className="modal-footer">
+					<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+)
+
+const DeleteModel = (props) => (
+	<div className="modal fade" id="myConfirmModal" tabIndex="-1" role="dialog" aria-labelledby="myConfirmLabel" aria-hidden="false">
+		<div className="modal-dialog">
+			<div className="modal-content">
+				<div className="modal-header">
+					<button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 className="modal-title" id="myModalLabel">{props.title}</h4>
+				</div>
+				<div className="modal-body">
+					<p>
+						{props.message}
+					</p>
+				</div>
+				<div className="modal-footer">
+					<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" className="btn btn-primary" onClick={props.onDeleteBucket} data-dismiss="modal">Delete</button>
+				</div>
+			</div>
+		</div>
+	</div>
+)
+
+export  {AddBucketModal, AlertModel, DeleteModel}
 
