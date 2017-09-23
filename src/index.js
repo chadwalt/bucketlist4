d8616fc5c804815ac8141965b39b2d4e96d30a1d
chadwalt <chadwalt04@gmail.com>
2017-09-23 07:48:54 +0300

    Add bucketitems container
    This will control the bucket items crud

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Login from "./containers/Login"
 import Signup from "./containers/Signup"
 import Dashboard from "./containers/Dashboard"
+import BucketItems from "./containers/BucketItems"
 import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render((
@@ -12,7 +13,8 @@ ReactDOM.render((
 		<Switch>
 			<Route exact path="/" component={Login}/>
 			<Route path="/signup" component={Signup}/>		
-			<Route path="/dashboard" component={Dashboard}/>			
+			<Route path="/dashboard" component={Dashboard}/>	
+			<Route path="/bucketitems/:id" component={BucketItems}/>		
 		</Switch>
 	</BrowserRouter>), document.getElementById("root"))
 registerServiceWorker()
