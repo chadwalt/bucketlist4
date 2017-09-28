bdc6a99256042a82be2ee48a5ec18ef545e7b423
chadwalt <chadwalt04@gmail.com>
2017-09-28 12:05:51 +0300

    Add tests for model components

---
+++ src/__test__/add_buckets_model.test.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { AddBucketModal, AlertModel, DeleteModel, AddBucketItemModal } from "../components/add_bucket_model"
+import { configure } from "enzyme"
+import Adapter from "enzyme-adapter-react-15"
+configure({ adapter: new Adapter() })
+
+import { shallow } from "enzyme"
+
+const props = {
+	title: "Add Buckets",
+	bucketName: "Going to the moon",
+}
+const bucket_modal = shallow(<AddBucketModal {...props} />)
+
+const alert_modal = shallow(<AlertModel {...props} />)
+
+const delete_modal = shallow(<DeleteModel {...props} />)
+
+const bucketItem_modal = shallow(<AddBucketItemModal {...props} />)
+
+describe("Test for <AddBucketModal /> component", () => {
+	it("It should render 6 divs for the add buckets", () => {
+		expect(bucket_modal.find("div").length).toEqual(6)
+	})
+
+	it ("It should render 6 divs for the alert model", () => {
+		expect(alert_modal.find("div").length).toEqual(6)
+	})
+
+	it ("It should render 6 divs for the Delete model", () => {
+		expect(delete_modal.find("div").length).toEqual(6)
+	})
+
+	it ("It should render 8 divs for the AddBucketlist model", () => {
+		expect(bucketItem_modal.find("div").length).toEqual(8)
+	})
+
+})
+
