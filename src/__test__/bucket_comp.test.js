d003c01990fd51092ff7f2c467141d4d2943da06
chadwalt <chadwalt04@gmail.com>
2017-09-30 10:39:25 +0300

    Add test for button clicks

--- src/__test__/bucket_comp.test.js
+++ src/__test__/bucket_comp.test.js
@@ -3,7 +3,7 @@ import Buckets from "../components/buckets"
 import "./setup"
 import renderer from 'react-test-renderer';
 
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 
 const props = {
 	displayTable: "buckets",
@@ -18,16 +18,17 @@ const props = {
 		id: 30,
 		name: "Traveling to mombosa",
 		user_id: 38
-	}, {
-		date_created: "Fri, 22 Sep 2017 18:09:55 GMT",
-		id: 31,
-		name: "Traveling to Usa",
-		user_id: 38
 	}]
 }
 
 const itemsProps = {
 	displayTable: "bucketItems",
+	pagination: {
+		"next_url": "",
+		"page": 1,
+		"pages": 0,
+		"prev_url": "",
+	},
 	buckets: [{
 		date_created: "Fri, 22 Sep 2017 11:09:55 GMT",
 		id: 30,
@@ -35,13 +36,6 @@ const itemsProps = {
 		user_id: 38,
 		description: "mkop",
 		bucket_id: 3,
-	}, {
-		date_created: "Fri, 22 Sep 2017 18:09:55 GMT",
-		id: 31,
-		name: "Chilling in mombosa",
-		user_id: 38,
-		description: "mkop",
-		bucket_id: 3
 	}]
 }
 
@@ -50,15 +44,33 @@ const bucket_comp = shallow(<Buckets {...props} />)
 const bucketitems_comp = shallow(<Buckets {...itemsProps} />)
 
 describe("It should render the Buckets component", () => {
-	it("It renders correctly", () =>{
+
+	it("It renders correctly", () => {
 		const rendered = renderer.create(<Buckets {...props} />)
 		expect(rendered.toJSON()).toMatchSnapshot()
 	})
+
 	it("It has 20 divs and renders the bucket list.", () => {
 		expect(bucket_comp.find("div").length).toEqual(20)
 	})
 
 	it("It has 20 divs and renders the bucketlist items list.", () => {
 		expect(bucketitems_comp.find("div").length).toEqual(20)
+	})
+
+	it("Should be able to click edit and delete bucket button", () => {
+		const mockFunction = jest.fn()
+		const element = mount(<Buckets {...props} onEdit={mockFunction} onOpenDelete={mockFunction}/>)
+		element.find("#editBucket").simulate("click")
+		element.find("#deleteBucket").simulate("click")
+		expect(mockFunction).toHaveBeenCalled()
+	})
+
+	it("Should be able to click edit bucket button", () => {
+		const mockFunction = jest.fn()
+		const element = mount(<Buckets {...itemsProps} onEdit={mockFunction} onOpenDelete={mockFunction}/>)
+		element.find("#editBucketItem").simulate("click")
+		element.find("#deleteBucketItem").simulate("click")
+		expect(mockFunction).toHaveBeenCalled()
 	})
 })
