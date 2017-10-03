f52092a80491fbb239c9ddb2a73188b0e7301623
chadwalt <chadwalt04@gmail.com>
2017-10-03 06:02:31 +0300

    Add tests

--- src/__test__/dashboard.test.js
+++ src/__test__/dashboard.test.js
@@ -10,13 +10,25 @@ import BaseUrl from "../config"
 
 import { shallow, mount } from "enzyme"
 const mock = new MockAdapter(axios)
-mock.onGet(BaseUrl+"bucketlists/?page=1&rows=8").reply(200,{})
+
+mock.onGet(BaseUrl + "bucketlists/?page=1&rows=8").reply(200, {})
+mock.onPost(BaseUrl + "auth/logout").reply(200, { success: true })
+mock.onDelete(BaseUrl + "bucketlists/1").reply(200, { success: true })
+mock.onGet(BaseUrl + "bucketlists/?q=Climbing&page=1&rows=8").reply(200, { success: true })
+mock.onGet(BaseUrl + "bucketlists/?page=1&rows=8").reply(200, {
+	buckets: {
+		id: 3,
+		name: "Visiting USA."
+	}
+})
+
 describe("Testing the Dashboard container", () => {
-	
+
 	const mockFunction = jest.fn()
-	// jest.mock("axios")
+	//let wrapper
+
 	const props = {
-		displayTable:"buckets",
+		displayTable: "buckets",
 		onLogout: mockFunction,
 		modelId: "#myModal",
 		onEdit: mockFunction,
@@ -29,10 +41,17 @@ describe("Testing the Dashboard container", () => {
 		dismissAlert: mockFunction
 	}
 
+	// afterEach(() => {
+	// 	wrapper = shallow(<Dashboard {...props} />)
+	// 	wrapper.setState({ bucket_name: "Joining the navy" })
+	// 	sessionStorage.setItem("auth_token", "382283jjdjasdfadfa")
+	// 	mock.onPost(BaseUrl + "bucketlists", wrapper.state().bucket_name).reply(200, { success: true })
+	// })
+
 	it("It calls the onclick event for the edit of the bucket.", () => {
 		const dashboard = mount(
 			<StaticRouter context={{}}>
-				<Dashboard {...props}/>
+				<Dashboard {...props} />
 			</StaticRouter>
 		)
 		expect(dashboard).toHaveLength(1)
@@ -41,9 +60,87 @@ describe("Testing the Dashboard container", () => {
 	it("renders correctly", () => {
 		const rendered = renderer.create(
 			<StaticRouter context={{}}>
-				<Dashboard {...props}/>
+				<Dashboard {...props} />
 			</StaticRouter>)
 		expect(rendered.toJSON()).toMatchSnapshot()
+	})
+
+	it("It should check change state", () => {
+		const wrapper = shallow(<Dashboard {...props} />)
+
+		sessionStorage.setItem("auth_token", "382283jjdjasdfadfa")
+		wrapper.instance().checkLoggedStatus()
+		expect(wrapper.state().isLoggedIn).toEqual(true)
+
+		wrapper.instance().onDismiss()
+		expect(wrapper.state().visible).toEqual(false)
+
+		wrapper.instance().editBucket("Edit Bucket", 1)
+		expect(wrapper.state().model_title).toEqual("Edit Bucket")
+
+		wrapper.instance().addBucket()
+		expect(wrapper.state().model_title).toEqual("Add Bucket")
+
+		wrapper.instance().openDeleteBucket("Climbing", 1)
+		expect(wrapper.state().bucket_name).toEqual("Climbing")
+
+		wrapper.instance().componentDidMount()
+	})
+
+	it("it should logout user", () => {
+		const wrapper = shallow(<Dashboard {...props} />)
+		const event = {
+			preventDefault: () => { }
+		}
+
+		wrapper.instance().logout(event)
+		expect(wrapper.state().redirect).toEqual("logout")
+	})
+
+	it("It should update state on form input.", () => {
+		const wrapper = shallow(<Dashboard {...props} />)
+		const event = {
+			preventDefault: () => { },
+			target: {
+				name: "buckets",
+				value: "Climbing"
+			}
+		}
+
+		wrapper.instance().handleInput(event)
+		expect(wrapper.state().buckets).toEqual("Climbing")
+	})
+
+	it("It should delete bucket.", () => {	
+		const wrapper = shallow(<Dashboard />)
+		sessionStorage.setItem("auth_token", "382283jjdjasdfadfa")	
+
+		wrapper.setState({bucket_id: 1})
+		wrapper.instance().deleteBucket()
+		expect(wrapper.state().buckets.length).toEqual(0)
+	})
+
+	it("It should move to next page.", () => {	
+		const wrapper = shallow(<Dashboard {...props} />)
+		sessionStorage.setItem("auth_token", "382283jjdjasdfadfa")	
+
+		wrapper.instance().movePage(1)
+		expect(wrapper.state().buckets.length).toEqual(0)
+	})
+
+	it("It should search through the buckets.", () => {	
+		const wrapper = shallow(<Dashboard {...props} />)
+		sessionStorage.setItem("auth_token", "382283jjdjasdfadfa")	
+
+		const event = {
+			preventDefault: () => { },
+			target: {
+				value: "Climbing"
+			}
+		}
+
+		wrapper.instance().searchBucket(event)
+		expect(wrapper.state().buckets.length).toEqual(0)
 	})
 
 })
--- src/__test__/login.test.js
+++ src/__test__/login.test.js
@@ -1,11 +1,61 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import Login from "../containers/Login"
+import "./setup"
+import BaseUrl from "../config"
+
+import { shallow, mount } from "enzyme"
+import axios from "axios"
+import MockAdapter from "axios-mock-adapter"
+const mock = new MockAdapter(axios)
+
+mock.onPost(BaseUrl + "auth/login", {username: "Chadwalt", password: "123"}).reply(200, { success: true })
 
 describe("Login Snapshot", () => {
 	it("renders correctly", ()=> {
 		const rendered = renderer.create(<Login/>)
 		expect(rendered.toJSON()).toMatchSnapshot()
 	})
+
+	it("It should check change state", () => {
+		const wrapper = shallow(<Login />)
+
+		wrapper.instance().onDismiss()
+		expect(wrapper.state().visible).toEqual(false)
+
+		const event = {
+			preventDefault: () => { },
+			target: {
+				name: "username",
+				value: "chad"
+			}
+		}
+		wrapper.instance().create_account(event)
+		expect(wrapper.state().redirect).toEqual("signup")
+	})
+
+	it("It should update state on form input.", () => {
+		const wrapper = shallow(<Login />)
+		const event = {
+			preventDefault: () => { },
+			target: {
+				name: "username",
+				value: "chad"
+			}
+		}
+
+		wrapper.instance().handle_input(event)
+		expect(wrapper.state().username).toEqual("chad")
+	})
+
+	// it("It should be to login user", () =>{
+	// 	const wrapper = shallow(<Login />)
+	// 	const event = {
+	// 		preventDefault: () => { },
+	// 	}
+
+	// 	wrapper.instance().login(event)
+	// 	expect(wrapper.state().alert_type).toEqual("danger")
+	// })
 
 })
