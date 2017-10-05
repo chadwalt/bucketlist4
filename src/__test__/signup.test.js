831068ccc861f1928dc35e17e14d8c810fa19a7e
chadwalt <chadwalt04@gmail.com>
2017-10-05 12:53:21 +0300

    Add register test.

--- src/__test__/signup.test.js
+++ src/__test__/signup.test.js
@@ -7,9 +7,20 @@ import BaseUrl from "../config"
 import { shallow, mount } from "enzyme"
 import axios from "axios"
 import MockAdapter from "axios-mock-adapter"
+import "url-search-params-polyfill"
 const mock = new MockAdapter(axios)
 
+mock.onPost(BaseUrl + "auth/register").reply(200, {success: true})
+
 describe("Login Snapshot", () => {
+	let params = {
+		first_name: "Timothy",
+		sur_name: "Kyadondo",
+		username: "chad",
+		password: "123",
+		email: "chadwalt@outlook.com"
+	}
+
 	it("renders correctly", ()=> {
 		const rendered = renderer.create(<Signup/>)
 		expect(rendered.toJSON()).toMatchSnapshot()
@@ -36,6 +47,21 @@ describe("Login Snapshot", () => {
 
 		wrapper.instance().handle_input(event)
 		expect(wrapper.state().username).toEqual("chad")
+	})
+
+	it("It should register the user.", () => {
+		const wrapper = shallow(<Signup />)
+		const event = {
+			preventDefault: () => { },
+			target: {
+				name: "username",
+				value: "chad"
+			}
+		}
+		wrapper.instance().create_account(event)
+		wrapper.setState(params)
+		wrapper.instance().create_account(event)
+		expect(wrapper.state().redirect).toEqual(false)
 	})
 
 })
