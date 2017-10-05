65f8c20656f2f9fdc756161e034c53300b419e01
chadwalt <chadwalt04@gmail.com>
2017-10-05 13:49:35 +0300

    Add test for user login

--- src/__test__/login.test.js
+++ src/__test__/login.test.js
@@ -7,11 +7,18 @@ import BaseUrl from "../config"
 import { shallow, mount } from "enzyme"
 import axios from "axios"
 import MockAdapter from "axios-mock-adapter"
+import "url-search-params-polyfill"
 const mock = new MockAdapter(axios)
 
-mock.onPost(BaseUrl + "auth/login", {username: "Chadwalt", password: "123"}).reply(200, { success: true })
+mock.onPost(BaseUrl + "auth/login").reply(200, { success: true })
 
 describe("Login Snapshot", () => {
+	let params = {
+		username: "chadwalt",
+		password: "123",
+		redirect: "",
+		visible: false
+	}
 	it("renders correctly", ()=> {
 		const rendered = renderer.create(<Login/>)
 		expect(rendered.toJSON()).toMatchSnapshot()
@@ -48,14 +55,16 @@ describe("Login Snapshot", () => {
 		expect(wrapper.state().username).toEqual("chad")
 	})
 
-	// it("It should be to login user", () =>{
-	// 	const wrapper = shallow(<Login />)
-	// 	const event = {
-	// 		preventDefault: () => { },
-	// 	}
+	it("It should be to login user", () =>{
+		const wrapper = shallow(<Login />)
+		const event = {
+			preventDefault: () => { },
+		}
 
-	// 	wrapper.instance().login(event)
-	// 	expect(wrapper.state().alert_type).toEqual("danger")
-	// })
+		wrapper.instance().login(event)
+		wrapper.setState(params)
+		wrapper.instance().login(event)
+		expect(wrapper.state().alert_type).toEqual("danger")
+	})
 
 })
