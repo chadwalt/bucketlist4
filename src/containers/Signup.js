b85de13a2d3151320963dd5e99036dcef868f84c
chadwalt <chadwalt04@gmail.com>
2017-10-27 16:10:03 +0300

    Restructure signup component

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -5,51 +5,28 @@ import { Redirect } from "react-router"
 import BaseUrl from "../config"
 
 class Signup extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			first_name: "",
-			sur_name: "",
-			username: "",
-			password: "",
-			email: "",
-			redirect: false
-		}
-		this.create_account = this.create_account.bind(this)
-		this.handle_input = this.handle_input.bind(this)
-		this.login = this.login.bind(this)
+	state = {
+		first_name: "",
+		sur_name: "",
+		username: "",
+		password: "",
+		email: "",
+		redirect: false
 	}
 
 	/* This will handle getting input from the form.
 	* @param event => Event fired when the input is changed in the form.
 	********************************************************************/
-	handle_input(event) {
+	handle_input = (event) => {
 		const name = event.target.name
 		const value = event.target.value
 		this.setState({ [name]: value })
 	}
 
-	render() {
-		const { redirect } = this.state
-
-		if (redirect) {
-			return (<Redirect to="/" />)
-		}
-
-		return (
-			<SignupForm
-				{...this.state}
-				onInput={this.handle_input}
-				onSubmit={this.create_account}
-				onLogin={this.login}
-			/>
-		)
-	}
-
 	/* This will handle creating the account for the user.
 	* @param e => Event, the event fired when the button is clicked.
 	* *******************************************************************/
-	create_account(e) {
+	create_account = (e) => {
 		e.preventDefault()
 
 		var params = new URLSearchParams() // Using the x-www-form-urlencoded.
@@ -79,7 +56,7 @@ class Signup extends Component {
 	/* This will handle navigating the user to the login page.
 	* @param e => Event, the event fired when the button is clicked.
 	* *******************************************************************/
-	login(e) {
+	login = (e) => {
 		e.preventDefault()
 
 		this.setState({
@@ -87,6 +64,22 @@ class Signup extends Component {
 		})
 	}
 
+	render() {
+		const { redirect } = this.state
+
+		if (redirect) {
+			return (<Redirect to="/" />)
+		}
+
+		return (
+			<SignupForm
+				{...this.state}
+				onInput={this.handle_input}
+				onSubmit={this.create_account}
+				onLogin={this.login}
+			/>
+		)
+	}
 
 }
 
