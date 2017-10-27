0b189550daa1634be977bf1e4e43aad610974259
chadwalt <chadwalt04@gmail.com>
2017-10-27 15:30:39 +0300

    Restructure Login component and add proptypes for the props.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,63 +6,32 @@ import axios from "axios"
 import LoginForm from "../components/login_form"
 import { Redirect } from "react-router"
 import BaseUrl from "../config"
-import { Alert } from 'reactstrap';
+import { Alert } from "reactstrap"
+import PropTypes from "prop-types"
 
 
 class Login extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			username: "",
-			password: "",
-			redirect: "",
-			visible: false
-		}
-		this.create_account = this.create_account.bind(this)
-		this.handle_input = this.handle_input.bind(this)
-		this.login = this.login.bind(this)
-		this.onDismiss = this.onDismiss.bind(this)
+	state = {
+		username: "",
+		password: "",
+		redirect: "",
+		visible: false
 	}
 
-	handle_input(event) {
+	/* This will handle inpput provided by the user.
+	* @param e => Event, the event fired when the text is changed is clicked.
+	* ***********************************************************************/
+	handle_input = (event) => {
 		const name = event.target.name
 		const value = event.target.value
 		this.setState({ [name]: value })
 	}
 
-	render() {
-		const { redirect } = this.state
-
-		if (redirect === "signup") {
-			return (<Redirect to="/signup" />)
-		} else if (redirect === "dashboard") {
-			return (<Redirect to="/dashboard" />)
-		}
-
-		return (
-			<div>
-				<Alert color={this.state.alert_type} isOpen={this.state.visible} toggle={this.onDismiss}>
-					<center>{this.state.message}</center>
-				</Alert>
-
-				<LoginForm
-					{...this.state}
-					onInput={this.handle_input}
-					onSubmit={this.login}
-					onSignup={this.create_account}
-				/>
-			</div>
-		)
-	}
-
-	onDismiss() {
-		this.setState({ visible: false })
-	}
 
 	/* This will handle loggin the user.
 	* @param e => Event, the event fired when the button is clicked.
 	* *******************************************************************/
-	login(e) {
+	login = (e) => {
 		e.preventDefault()
 
 		var params = new URLSearchParams() // Using the x-www-form-urlencoded.
@@ -95,16 +64,57 @@ class Login extends Component {
 			}
 		}.bind(this))
 	}
+
 	/* This will handle navigating the user to the signup page.
 	* @param e => Event, the event fired when the button is clicked.
 	* *******************************************************************/
-	create_account(e) {
+	create_account = (e) => {
 		e.preventDefault()
 
 		this.setState({
 			redirect: "signup"
 		})
 	}
+
+	onDismiss = () => {
+		this.setState({ visible: false })
+	}
+
+	render() {
+		const { redirect } = this.state
+
+		if (redirect === "signup") {
+			return (<Redirect to="/signup" />)
+		} else if (redirect === "dashboard") {
+			return (<Redirect to="/dashboard" />)
+		}
+
+		return (
+			<div>
+				<Alert color={this.state.alert_type} isOpen={this.state.visible} toggle={this.onDismiss}>
+					<center>{this.state.message}</center>
+				</Alert>
+
+				<LoginForm
+					{...this.state}
+					onInput={this.handle_input}
+					onSubmit={this.login}
+					onSignup={this.create_account}
+				/>
+			</div>
+		)
+	}
+
+}
+
+// Define prop types for the props.
+Login.PropTypes = {
+	color: PropTypes.string,
+	isOpen: PropTypes.bool,
+	onDismiss: PropTypes.func,
+	onInput: PropTypes.func,
+	onSubmit: PropTypes.func,
+	onSignup: PropTypes.func,
 }
 
 export default Login
