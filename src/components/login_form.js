174b0ffa3f0fb939cebdfd407f3847784eb85aa8
chadwalt <chadwalt04@gmail.com>
2017-10-27 15:50:37 +0300

    Change modal title and button color

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -6,7 +6,7 @@ const LoginForm = (props) => (
 			<div className="col-md-4 col-md-offset-4">
 				<div className="login-panel panel panel-default">
 					<div className="panel-heading">
-						<h3 className="panel-title">Please Sign In</h3>
+						<h3 className="panel-title">Sign In</h3>
 					</div>
 					<div className="panel-body">
 						<form role="form">
@@ -17,9 +17,8 @@ const LoginForm = (props) => (
 								<div className="form-group">
 									<input className="form-control" placeholder="Password" onChange={props.onInput} name="password" type="password" />
 								</div>
-								
-								<a href="index.html" className="btn btn-lg btn-success btn-block" onClick={props.onSubmit} >Login</a>
-								<a href="index.html" className="btn btn-lg btn-success btn-block" onClick={props.onSignup}>Create Account</a>
+								<a href="index.html" className="btn btn-lg btn-primary btn-block" onClick={props.onSubmit} >Login</a>
+								<a href="index.html" className="btn btn-lg btn-primary btn-block" onClick={props.onSignup}>Create Account</a>
 							</fieldset>
 						</form>
 					</div>
